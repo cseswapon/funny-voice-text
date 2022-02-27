56d2ee2b35dbb32cde650b5003d6a5418b8c16ae
cseswapon <cseswaponsaha@gmail.com>
2022-02-27 20:32:23 +0600

    [ADD] - Spling Check

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let myFun = new webkitSpeechRecognition();
-myFun.onresult = (e)=>{
-    let text = e.results[0][0].transcript;
-    document.getElementById('text').innerText=text;
-    read(text)
-}
-function read(text){
-    let speak = new SpeechSynthesisUtterance();
-    speak.text = text;
-    if (text=='hello') {
-        speak.text ='I am fuck you baby'
-    }else if (text == "what is your name") {
-      speak.text = "My Name is programing hero. I am your Assistants";
-    } else if (text == "what is your father name name") {
-      speak.text = "My Father Name is Swapon Saha";
-    } else if (text == "what is your bet language") {
-      speak.text = "Javascript is my favorite language";
-    } else {
-      speak.text = "please try again";
-    }
-    document.getElementById('res').innerText=speak.text
-    window.speechSynthesis.speak(speak);
+myFun.onresult = (e) => {
+  let text = e.results[0][0].transcript;
+  document.getElementById("text").innerText = text;
+  read(text);
+};
+function read(text) {
+  let speak = new SpeechSynthesisUtterance();
+  speak.text = text;
+  if (text == "hello") {
+    speak.text = "I am fuck you baby";
+  } else if (text == "what is your name") {
+    speak.text = "My Name is programing hero. I am your Assistants";
+  } else if (text == "what is your father name") {
+    speak.text = "My Father Name is Swapon Saha";
+  } else if (text == "what is your best language") {
+    speak.text = "Javascript is my favorite language";
+  } else {
+    speak.text = "please try again";
+  }
+  document.getElementById("res").innerText = speak.text;
+  window.speechSynthesis.speak(speak);
 }
